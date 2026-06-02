7f8c7c4db180ea59e0dd24b0d709c6bf69821fff
vishubth <59112242+vishubth@users.noreply.github.com>
2026-06-02 12:04:02 +0530

    refactor: add blockchain service module

---
+++ src/services/blockchain_service.py
@@ -0,0 +1,10 @@
+import requests
+
+class BlockchainService:
+    @staticmethod
+    def get_eth_price():
+        return 'migrate logic from main.py'
+
+    @staticmethod
+    def verify_transaction():
+        return 'migrate logic from main.py'
